5bd4effcdfc9e5862c75259b3d8ec6e3a6dbebe8
jakemeout <jacobosity@gmail.com>
2020-11-16 17:15:40 -0500

    fixed  xhr cors issue

--- server/index.js
+++ server/index.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
 
-const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
 
 const router = require('./router')
 
-const app = express();
-const server = http.createServer(app);
+
+const server =  require('http').createServer(app)
 const io = socketio(server);
+
+
+app.use(router);
+
 
 io.on('connection',(socket) => {
     console.log("We have a new connection.");
-
+    
     socket.on('disconnect', () => {
         console.log('User had left!!!');
     })
 });
 
-app.use(router);
 
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
 
--- server/router.js
+++ server/router.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('server is up and running');
+router.get("/", (req, res) => {
+  res.send({ response: "Server is up and running." }).status(200);
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
 
 module.exports = router;
