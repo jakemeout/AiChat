cead54a199261b16fe7b0f96baa29497b4b32044
jakemeout <jacobosity@gmail.com>
2020-11-16 17:15:10 -0500

    fixed xhr cors issue

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,16 +2,30 @@ import React, {useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+let socket;
 
-const Chat = ({location}) => {
-    useEffect(() =>
-    const {data}= queryString.parse(location.search);
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000'
+
+    useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+   
+    socket = io(ENDPOINT, {transports: ['websocket', 'polling', 'flashsocket']});
+
+    setName(name);
+    setRoom(room);
     
-    console.log(location.search);
-    console.log(data);
-    )
+    console.log(socket);
+    socket.emit('join', { name, room }, (error) => {
+        if(error) {
+          alert(error);
+        }
+      });
+    }, [ENDPOINT, location.search]);
 
-
+   
     return (
         <h1>Chat</h1>
     )
