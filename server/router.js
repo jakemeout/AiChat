722c55af7b18984e1e7ec9e8b90b538b3855f874
jakemeout <jacobosity@gmail.com>
2021-01-20 17:19:46 -0500

    updated server

--- server/router.js
+++ server/router.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send({ response: "Server is up and running." }).status(200);
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
 
 module.exports = router;
--- server/users.js
+++ server/users.js
@@ -1,17 +1,31 @@
 const users = [];
 
-const addUser = ({id, name, room}) => {
-    
-}
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
 
-const removeUser = () => {
+  const existingUser = users.find(
+    () => user.room === room && user.name === name
+  );
 
-}
+  if (!name || !room) return { error: "Username and room are required." };
+  if (existingUser) return { error: "Username is taken." };
 
-const getUser = () => {
+  const user = { id, name, room };
 
-}
+  users.push(user);
 
-const getUsersInRoom = () => {
+  return { user };
+};
 
-}
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) return users.splice(index, 1)[0];
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
